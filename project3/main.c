9dd2377aa11bf50aaf1ae6a682a0fecb21fb5cf7
dirk <dirkadin@gmail.com>
2018-11-13 09:21:34 -0600

    Initial commit, need to add malloc

--- project3/main.c
+++ project3/main.c
@@ -9,7 +9,29 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
+	
+	int size = 0;
+	
+	printf("This program lets the user create a Binary Search Tree or a Heap.\n\n");
+	printf("It asks the user to enter the number of nodes to be created.\n\n");
+	printf("Then it asks the user to enter the data values for thespecified number ofnodes.\n\n");
+	printf("Then it asks the user to enter the data values for thespecified number ofnodes.\n\n");
+	printf("-------------------------------------------------------------------------------------------\n\n");
+	
+	printf("Enter the number of nodes in the structureto be initially created: ");
+	scanf("%d", &size);
+	
+	int myNum[size];
+	for (int i = 0; i < size; i++) {
+		printf("\n\nEnter the data value to be placed in the node: ");
+		scanf("%d", &myNum[i]);
+	}
+	
+	char *selection = NULL;
+	printf("What do you wish to create –a Binary search Tree (T) or a Min-Heap (H)?: ");
+	scanf("%c", selection);
+	
+	printf("\n\n%s", selection);
+	
 	return 0;
 }
